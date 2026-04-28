bc5c3032d830a0887cfd33852fcbd48f7b995659
Nyquil <jm2779736@gmail.com>
2026-04-27 23:22:28 -0400

    created program

---
+++ src/Character.java
@@ -0,0 +1,5 @@
+public class Character {
+    public Character(String Name,String Element, String wepType){
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.random.RandomGenerator;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Genshin Reactions Assignment \n [1] Start Game \n [2] Settings \n [3] Quit");
+        String menu = input.nextLine();
+    }
+}
